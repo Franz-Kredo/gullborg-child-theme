6d58d3d5acefb389e6700a5bd310b3a3da85675a
Franz Isak <franz@kredo.is>
2025-01-02 14:11:13 +0000

    Removing opening times on polish, should be found on website instead

--- woocommerce/emails/customer-processing-order.php
+++ woocommerce/emails/customer-processing-order.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Customer processing order email
  *
@@ -15,19 +16,19 @@
  * @version 3.7.0
  */
 
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
+if (! defined('ABSPATH')) {
+    exit;
 }
 
 /*
  * @hooked WC_Emails::email_header() Output the email header
  */
-do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
+do_action('woocommerce_email_header', $email_heading, $email); ?>
 
 <?php /* translators: %s: Customer first name */ ?>
-<p><?php printf( esc_html__( 'Hi %s,', 'woocommerce' ), esc_html( $order->get_billing_first_name() ) ); ?></p>
+<p><?php printf(esc_html__('Hi %s,', 'woocommerce'), esc_html($order->get_billing_first_name())); ?></p>
 <?php /* translators: %s: Order number */ ?>
-<p><?php printf( esc_html__( 'Við höfum fengið pöntun númer #%s, og við erum að taka hana saman! Leiðbendingar hér fyrir neðan. (język polski na dole)', 'woocommerce' ), esc_html( $order->get_order_number() ) ); ?></p>
+<p><?php printf(esc_html__('Við höfum fengið pöntun númer #%s, og við erum að taka hana saman! Leiðbendingar hér fyrir neðan. (język polski na dole)', 'woocommerce'), esc_html($order->get_order_number())); ?></p>
 
 <?php
 
@@ -37,41 +38,43 @@ do_action( 'woocommerce_email_header', $email_heading, $email ); ?>
  * @hooked WC_Structured_Data::output_structured_data() Outputs structured data.
  * @since 2.5.0
  */
-do_action( 'woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email );
+do_action('woocommerce_email_order_details', $order, $sent_to_admin, $plain_text, $email);
 
 /*
  * @hooked WC_Emails::order_meta() Shows order meta data.
  */
-do_action( 'woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email );
+do_action('woocommerce_email_order_meta', $order, $sent_to_admin, $plain_text, $email);
 
 /*
  * @hooked WC_Emails::customer_details() Shows customer details
  * @hooked WC_Emails::email_address() Shows email address
  */
-do_action( 'woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email );
+do_action('woocommerce_email_customer_details', $order, $sent_to_admin, $plain_text, $email);
 
 /**
  * Show user-defined additional content - this is set in each email's settings.
  */
-if ( $additional_content ) {
-	echo wp_kses_post( wpautop( wptexturize( $additional_content ) ) );
+if ($additional_content) {
+    echo wp_kses_post(wpautop(wptexturize($additional_content)));
 }
 
 /* FRANZ */
 ?>
-<br><hr><h2>Język polski</h2>
+<br>
+<hr>
+<h2>Język polski</h2>
 <p>Zbieramy Twoje zamówienie i będzie ono gotowe w ciągu godziny, jeżeli zostało złożone w godzinach pracy. Zamówienie znajduje się pod adresem Engjaveg 7, 104 Reykjavík, gdzie dostawą zajmuje się Gullborg Flugeldar.
 
-Uwaga: jeśli złożyłeś zamówienie tuż przed zamknięciem, istnieje mała szansa, że ​​Twoje zamówienie nie będzie gotowe przed zamknięciem. Jeśli wystąpi problem, można zadzwonić do Vignir lub Magnús.<br>Vignir: 698-6620<br>Magnús: 696-1770 <br>(tylko islandzki i angielski)</p>
+    Uwaga: jeśli złożyłeś zamówienie tuż przed zamknięciem, istnieje mała szansa, że ​​Twoje zamówienie nie będzie gotowe przed zamknięciem. Jeśli wystąpi problem, można zadzwonić do Vignir lub Magnús.<br>Vignir: 698-6620<br>Magnús: 696-1770 <br>(tylko islandzki i angielski)</p>
 
-<h2>Godziny otwarcia</h2>
+<!-- <h2>Godziny otwarcia</h2>
                     <p><b>28-30.grudzień</b><br>10:00 - 22:00</p> 
                     <p><b>31.grudzień</b><br>10:00 - 16:00</p> 
                     <p><b>2-5. styczeń</b><br>14:00 - 18:00</p> 
-                    <p><b>6.styczeń</b><br>10:00 - 18:00</p> 
+                    <p><b>6.styczeń</b><br>10:00 - 18:00</p>  -->
 
 <?php
 /*
  * @hooked WC_Emails::email_footer() Output the email footer
  */
-do_action( 'woocommerce_email_footer', $email );
+do_action('woocommerce_email_footer', $email);
